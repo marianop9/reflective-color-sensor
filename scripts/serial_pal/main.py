491f55fc9b610555f78fbeb1465f04ed2f14b58f
Mariano Perez <perezm7401@gmail.com>
2025-11-27 15:34:51 -0300

    serial tool

---
+++ scripts/serial_pal/main.py
@@ -0,0 +1,13 @@
+from serial_pal import SerialPal
+
+
+def main():
+    try:
+        SerialPal().cmdloop()
+    except KeyboardInterrupt:
+        print("exiting...")
+        SerialPal.cleanup()
+
+
+if __name__ == "__main__":
+    main()
