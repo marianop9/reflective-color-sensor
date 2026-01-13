ad9a64eb73903848e87bf8d05434acba8e75897a
Mariano Perez <perezm7401@gmail.com>
2026-01-13 17:24:13 -0300

    comunicacion serial - mensaje de respuesta

--- firmware/src/lib/ascii-communcation-protocol/include/serial_com.h
+++ firmware/src/lib/ascii-communcation-protocol/include/serial_com.h
@@ -1,6 +1,29 @@
 /** Code based on the "ascii-communication-protocol" library by "esadigov".
- * 
+ *
  * https://github.com/esadigov/ascii-communication-protocol
+ */
+
+/** Expected formats:
+ * 
+ * COMMAND
+ * (1) ACP_START_CHAR           (&)
+ * (2) CMD_STR                  req. max 10 chars
+ * (3) ACP_SEPARATOR_CHAR       (,)
+ * (4) PAYLOAD_1                opt. max 10 chars
+ * (5) ACP_SEPARATOR_CHAR       (,)
+ * (6) PAYLOAD_2                opt. max 10 chars
+ * (7) ACP_END_CHAR             (*)
+ * (8) CHECKSUM                 req. 8-bit checksum
+ * 
+ * RESPONSE
+ * Actually not ASCII-encoded, but easier to work with.
+ * (1) ACP_START_CHAR           (&)
+ * (2) RESP_STR                 req. ASCII chars
+ * (3) ACP_SEPARATOR_CHAR       (,)
+ * (4) PAYLOAD_LENGTH_BYTES     req. (specified in BYTES) raw, 8-bit number
+ * (5) ACP_SEPARATOR_CHAR       (,)
+ * (6) PAYLOAD                  req. raw data. length is PAYLOAD_LENGTH bytes.
+ * (7) ACP_END_CHAR             (*)
  */
 
 #ifndef ACP_SERIAL_COM_H
@@ -15,7 +38,7 @@
 /**< Character that signifies the end of a command. */
 #define ACP_END_CHAR '*'
 /**< Character used to separate commands and payloads. */
-#define ACP_CMD_SEPARATOR ','
+#define ACP_SEPARATOR_CHAR ','
 
 // Sizes and definitions
 /** Command buffer size. */
@@ -32,33 +55,58 @@
 #define ACP_PAYLOAD2_SIZE 10
 /** Maximum size for the checksum string. */
 #define ACP_CHECKSUM_STR_SIZE 10
-
-/**
- * @brief List of available commands as strings.
- */
-const char *ACP_COMMAND_LIST[] = {"COMMAND1", "COMMAND2"};
+/** Maximum size for response payload (in bytes) */
+#define ACP_RESP_PAYLOAD_SIZE 128
 
 /**
  * @brief Enum defining the available commands.
- * 
+ *
  * First command must have value `0`.
  */
 typedef enum {
-    COMMAND1 = 0,     /**< Command 1. */
-    COMMAND2,     /**< Command 2. */
-    NUM_COMMANDS, /**< Total number of commands. */
-    INVALID_CMD   /**< Invalid command. */
+    ACP_CMD_PING = 0,
+    ACP_CMD_MEM,
+    ACP_CMD_TOGGLE_LED, // test onboard led
+    ACP_CMD_ADC,
+    ACP_CMD_SET_LED,
+    ACP_CMD_COUNT,  /**< Total number of commands. */
+    ACP_CMD_INVALID /**< Invalid command. */
 } acp_command_type_t;
 
 /**
  * @brief Structure representing a serial communication command.
  */
 typedef struct {
-    acp_command_type_t cmd;           /**< The command type. */
+    acp_command_type_t type;           /**< The command type. */
     char payload1[ACP_PAYLOAD1_SIZE]; /**< The first payload data. */
     char payload2[ACP_PAYLOAD2_SIZE]; /**< The second payload data. */
     uint8_t checksum; /**< Checksum for data integrity verification. */
 } acp_command_t;
+
+/**
+ * @brief Enum defining response types.
+ */
+typedef enum {
+    ACP_RESP_ACK = 0,
+    ACP_RESP_TEXT,
+    ACP_RESP_DATA,
+    ACP_RESP_COUNT,
+} acp_response_type_t;
+
+/**
+ * @brief Struct representing a serial communication response.
+ *
+ * The payload is sent as raw binary data.
+ */
+typedef struct {
+    acp_response_type_t type;
+    uint8_t len_bytes;
+    /** Raw response payload. 
+     * If original size is larger than 8-bits, the caller is responsible for
+     * proper alignment, endianness, etc.
+     */
+    uint8_t payload[ACP_RESP_PAYLOAD_SIZE];
+} acp_response_t;
 
 /**
  * @brief Creates a command string for serial communication from a acp_command_t
@@ -68,7 +116,7 @@ typedef struct {
  * @param cmd The `acp_command_t` structure value.
  * @return true if creation was successful, false otherwire.
  */
-bool acp_create_command(char *buffer, acp_command_t cmd);
+// bool acp_create_command(char *buffer, acp_command_t cmd);
 
 /**
  * @brief Parses a command string and fills a acp_command_t structure.
@@ -91,5 +139,15 @@ bool acp_parse_command(acp_command_t *cmd, const char *buffer, int length);
  * @return true if the command is valid, false otherwise.
  */
 bool acp_is_valid_command(const acp_command_t *cmd);
+
+/**
+ * @brief Creates a response string for serial communication from a
+ * `acp_response_t` structure.
+ *
+ * @param buffer The buffer to store the formatted response.
+ * @param cmd Pointer to the `acp_response_t` structure.
+ * @return true if creation was successful, false otherwise.
+ */
+bool acp_create_response(uint8_t *buffer, acp_response_t *resp);
 
 #endif // ACP_SERIAL_COM_H
--- firmware/src/lib/ascii-communcation-protocol/tests/test_comm_protocol.c
+++ firmware/src/lib/ascii-communcation-protocol/tests/test_comm_protocol.c
@@ -1,45 +1,76 @@
+/** TODO!
+ * 
+ * Hacer un test como la gente con cmake+unity
+ */
+
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "serial_com.h"
 
+extern uint8_t calculate_checksum(const char *);
+
 void cmd_without_checksum(char *str);
 
 void test_create_and_parse_command() {
-  acp_command_t cmd = {
-      .cmd = COMMAND1, .payload1 = "payload1", .payload2 = "payload2"};
+    acp_command_t cmd = {
+        .type = ACP_CMD_PING, .payload1 = "payload1", .payload2 = "payload2"};
 
-  char buffer[ACP_CMD_BUFFER_SIZE] = {0};
+    char buffer[ACP_CMD_BUFFER_SIZE] = {0};
 
-  bool success = create_command(buffer, cmd);
-  assert(success && "Failed to create command.");
+    bool success = acp_create_command(buffer, cmd);
+    assert(success && "Failed to create command.");
 
-  acp_command_t parsed_cmd;
-  success = parse_command(&parsed_cmd, buffer, strlen(buffer));
-  assert(success && "Failed to parse command.");
-  
-  cmd_without_checksum(buffer);
-  const uint8_t checksum = calculate_checksum(buffer);
+    acp_command_t parsed_cmd;
+    success = acp_parse_command(&parsed_cmd, buffer, strlen(buffer));
+    assert(success && "Failed to parse command.");
 
-  assert(parsed_cmd.cmd == cmd.cmd);
-  assert(strcmp(parsed_cmd.payload1, cmd.payload1) == 0);
-  assert(strcmp(parsed_cmd.payload2, cmd.payload2) == 0);
-  assert(parsed_cmd.checksum == checksum);
+    cmd_without_checksum(buffer);
+    const uint8_t checksum = calculate_checksum(buffer);
+
+    assert(parsed_cmd.type == cmd.type);
+    assert(strcmp(parsed_cmd.payload1, cmd.payload1) == 0);
+    assert(strcmp(parsed_cmd.payload2, cmd.payload2) == 0);
+    assert(parsed_cmd.checksum == checksum);
+}
+
+void test_create_response() {
+    char test_payload[] = "response123";
+    int test_payload_len = strlen(test_payload);
+    
+    acp_response_t resp = {.type = ACP_RESP_TEXT};
+    memcpy(resp.payload, test_payload, test_payload_len);
+    resp.len_bytes = test_payload_len;
+
+    uint8_t buffer[128] = {0};
+
+    bool success = acp_create_response(buffer, &resp);
+    assert(success && "Failed to create response.");
+
+    assert(buffer[0] == ACP_START_CHAR);
+    assert(strncmp(buffer+1, "TEXT", 4) == 0);
+    assert(buffer[1+4] == ',');
+    assert(buffer[5+1] == resp.len_bytes);
+    assert(buffer[6+1] == ',');
+    assert(strncmp(buffer+7+1,  test_payload, test_payload_len) == 0);
+    assert(buffer[8+test_payload_len] == ACP_END_CHAR);
 }
 
 int main() {
-  test_create_and_parse_command();
+    test_create_and_parse_command();
 
-  printf("All tests passed.\n");
-  return 0;
+    test_create_response();
+
+    printf("All tests passed.\n");
+    return 0;
 }
 
 void cmd_without_checksum(char *str) {
-  size_t len = strlen(str);
-  if (len > 2) {
-    str[len - 2] = '\0';
-  } else if (len > 0) {
-    str[0] = '\0';
-  }
+    size_t len = strlen(str);
+    if (len > 2) {
+        str[len - 2] = '\0';
+    } else if (len > 0) {
+        str[0] = '\0';
+    }
 }
